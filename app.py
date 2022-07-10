628a82e6cc1b7093df734b4069b9e051cf4d8f3d
Utkarsh <utkarshprap@gmail.com>
2022-07-11 04:52:38 +0530

    new

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-import git
 popular_df = pickle.load(open("popular.pkl", "rb"))
 pt = pickle.load(open("pt.pkl", "rb"))
 books = pickle.load(open("books.pkl", "rb"))
 similarity_score = pickle.load(open("similarity_score.pkl", "rb"))
 app = Flask(__name__)
 
-@app.route('/update_server', methods=['POST'])
-def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('path/to/git_repo')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
 
 @app.route("/")
 def index():
