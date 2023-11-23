76631ef80db6090b65300112e0c9b832b1043eb4
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-23 23:42:56 +0530

    removed deprecations

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-popular_df = pickle.load(open("popular.pkl", "rb"))
+a=open("popular.pkl", "rb")
+popular_df = pickle.load(a)
 pt = pickle.load(open("pt.pkl", "rb"))
 books = pickle.load(open("books.pkl", "rb"))
 similarity_score = pickle.load(open("similarity_score.pkl", "rb"))
